967d94b86b9c58510399dde68ac13b66b84c2f14
Jhonatan Santiago <jhonssantiago@gmail.com>
2022-11-10 14:56:29 -0300

    consumindo detalhes do pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,16 @@
 const pokeApi = {}
 
-pokeApi.getPokemon = (offset = 0, limit = 20) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemon = (offset = 0, limit = 12) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit})`
     //solicitando e recebendo requisição
     return fetch(url)
             .then((response) => response.json()) //transforma em json
-            .then((jsonBody) => jsonBody.results)
-            .catch((error) => console.log(error))
+            .then((jsonBody) => jsonBody.results) //pega a lista de pokemon do result
+            .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) // lista de requsições dos detalhes do pkeomon
+            .then((detailsRequest) => Promise.all(detailsRequest)) //agurando todas as requisições de detalhes
+            .then((pokemonDetails) => pokemonDetails) //retornando os detalhes
 }
